367429aa8d3628dbd8553456e63a11042a3f5980
Brian Izaya <bizaya@teravisiongames.com>
2015-11-12 19:47:26 -0430

    Movimiento

--- GameJam/Assets/movimientoRapido.cs
+++ GameJam/Assets/movimientoRapido.cs
@@ -4,46 +4,36 @@ using UnityEngine.Networking;
 
 public class movimientoRapido : NetworkBehaviour
 {
-    public int m_PlayerNumber = 1;                // Used to identify which tank belongs to which player.  This is set by this tank's manager.
-    public int m_LocalID = 1;
-    public float m_Speed = 12f;                   // How fast the tank moves forward and back.
-    public float m_TurnSpeed = 180f;              // How fast the tank turns in degrees per second.
-    public float m_PitchRange = 0.2f;             // The amount by which the pitch of the engine noises can vary.
-    public AudioSource m_MovementAudio;           // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
-    public AudioClip m_EngineIdling;              // Audio to play when the tank isn't moving.
-    public AudioClip m_EngineDriving;             // Audio to play when the tank is moving.
-    public ParticleSystem m_LeftDustTrail;        // The particle system of dust that is kicked up from the left track.
-    public ParticleSystem m_RightDustTrail;       // The particle system of dust that is kicked up from the rightt track.
-    public Rigidbody m_Rigidbody;              // Reference used to move the tank.
 
-    private string m_MovementAxis;              // The name of the input axis for moving forward and back.
-    private string m_TurnAxis;                  // The name of the input axis for turning.
-    private float m_MovementInput;              // The current value of the movement input.
-    private float m_TurnInput;                  // The current value of the turn input.
-    private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
+    Vector3 movement;
+    public float speed = 6f;
+    Rigidbody playerRigidbody;
 
     private void Start()
     {
-        // The axes are based on player number.
-        m_MovementAxis = "Vertical";// + (m_LocalID + 1);
-        m_TurnAxis = "Horizontal";// + (m_LocalID + 1);
+
 
     }
 
     private void Update()
     {
-        if (!isLocalPlayer)
+        /*if (!isLocalPlayer)
             return;
+            */
+        float h = Input.GetAxisRaw("Horizontal1");
+        float v = Input.GetAxisRaw("Vertical1");
 
-        // Store the value of both input axes.
-        if (Input.GetKey(KeyCode.W))
-        {
-
-            this.transform.position = new Vector3(this.transform.position.x - 1, this.transform.position.y, this.transform.position.z);
-        }
-
-        //m_MovementInput = Input.GetAxis(m_MovementAxis);
-        //m_TurnInput = Input.GetAxis(m_TurnAxis);
+        Move(h, v);
 
     }
+
+    void Move(float h, float v)
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
 }
