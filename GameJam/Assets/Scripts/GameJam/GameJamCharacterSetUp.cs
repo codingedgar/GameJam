ce5d86d98c614ff559e9677ecd2a29421b2dbf94
Edgar JRG <erodriguez@teravisiongames.com>
2015-11-13 00:29:07 -0430

    quitar camara del player  y controles de input;

---
+++ GameJam/Assets/Scripts/GameJam/GameJamCharacterSetUp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.Networking;
+
+public class GameJamCharacterSetUp : NetworkBehaviour
+{
+
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+
+
+        activateInputControllers(this.gameObject, isLocalPlayer);
+        activateCamera(this.gameObject, isLocalPlayer);
+    }
+
+    static public void activateCamera(GameObject chobj, bool value)
+    {
+        chobj.GetComponentInChildren<Camera>().enabled = value;
+    }
+
+    static public void activateInputControllers(GameObject characterObject, bool value)
+    {
+        characterObject.GetComponent<FirstPersonController>().enabled = value;
+    }
+}
