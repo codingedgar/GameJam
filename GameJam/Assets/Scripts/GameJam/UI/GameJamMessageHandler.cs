eab560dea453ebc0a35999275aa45fc8781f08a7
amonascal <amonascal@teravisiongames.com>
2015-11-13 14:03:54 -0430

    Messages2

--- GameJam/Assets/Scripts/GameJam/UI/GameJamMessageHandler.cs
+++ GameJam/Assets/Scripts/GameJam/UI/GameJamMessageHandler.cs
@@ -5,7 +5,6 @@ using UnityEngine.Networking;
 
 public class GameJamMessageHandler : NetworkBehaviour
 {
-
 	public Text messageHolder;
 
 	public float lerpingTime = 1;
@@ -18,44 +17,46 @@ public class GameJamMessageHandler : NetworkBehaviour
 
 	public void Start() {
 		//Gets the show and hide position
-		showPosition = transform.localPosition;
-		hidePosition = transform.localPosition + Vector3.down * 100;
+		showPosition = transform.localPosition + Vector3.up * 100;
+		hidePosition = transform.localPosition;
 		transform.localPosition = hidePosition;
 		_lerpingTime = 1f / lerpingTime;
 	}
 
+	[ClientCallback]
 	public void Update() {
-		
+
 		if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
-			receiveMessage("Follow Me!");
+			CmdReceiveMessage("Follow Me!");
 
 		if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
-			receiveMessage("Whats This?");
+			CmdReceiveMessage("Whats This?");
 
 		if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
-			receiveMessage("Action!");
+			CmdReceiveMessage("Action!");
 
 		if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
-			receiveMessage("My Bad XD");
+			CmdReceiveMessage("My Bad XD");
 
 		if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5))
-			receiveMessage("NO!!!");
+			CmdReceiveMessage("NO!!!");
 
 		if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6))
-			receiveMessage("Pushing Box");
+			CmdReceiveMessage("Pushing Box");
 
 		if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7))
-			receiveMessage("Broken Wall");
+			CmdReceiveMessage("Broken Wall");
 
 		if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8))
-			receiveMessage("Moving Platform");
+			CmdReceiveMessage("Moving Platform");
 
 		if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
-			receiveMessage("Portal");
+			CmdReceiveMessage("Portal");
 
 	}
-
-	public void receiveMessage(string message) {
+	
+	[Command]
+	public void CmdReceiveMessage(string message) {
 		messageHolder.text = message;
 		StopAllCoroutines();
 		transform.localPosition = hidePosition;
@@ -67,17 +68,17 @@ public class GameJamMessageHandler : NetworkBehaviour
 		while(time < lerpingTime)
 		{
 			time += Time.deltaTime;
-			transform.position = Vector3.Lerp(hidePosition, showPosition, time * _lerpingTime);
+			transform.localPosition = Vector3.Lerp(hidePosition, showPosition, time * _lerpingTime);
 			yield return 0;
 		}
-		transform.position = showPosition;
+		transform.localPosition = showPosition;
 		yield return new WaitForSeconds(showedTime);
 		while (time > 0)
 		{
 			time -= Time.deltaTime;
-			transform.position = Vector3.Lerp(hidePosition, showPosition, time * _lerpingTime);
+			transform.localPosition = Vector3.Lerp(hidePosition, showPosition, time * _lerpingTime);
 			yield return 0;
 		}
-		transform.position = hidePosition;
+		transform.localPosition = hidePosition;
 	}
 }
