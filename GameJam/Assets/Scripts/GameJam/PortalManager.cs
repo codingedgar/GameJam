60ecad91ecc925fbf515b6e18e4f9b7a34b89a16
Brian Izaya <bizaya@teravisiongames.com>
2015-11-13 03:45:31 -0430

    portals

--- GameJam/Assets/Scripts/GameJam/PortalManager.cs
+++ GameJam/Assets/Scripts/GameJam/PortalManager.cs
@@ -1,14 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PortalManager : MonoBehaviour {
 
+    private List<GameObject> portalsElement;
+    public static PortalManager instance;
+
+    void Awake() {
+
+        instance = this;
+        portalsElement = new List<GameObject>();
+
+    }
 
 	void Start () {
 	
+
+
 	}
 	
 	void Update () {
 	
 	}
+
+    public static void AddPortals(GameObject portal)
+    {
+
+        instance.portalsElement.Add(portal);
+
+    }
+
+    public static void ActivatePortal(GameObject portalEnter, GameObject userPortal){
+
+        int portalIndexOut = portalEnter.GetComponent<Portals>().portalId;
+        GameObject portalOut = instance.portalsElement[portalIndexOut + 1];
+        userPortal.transform.position = portalOut.transform.position;
+    }
 }
---
+++ GameJam/Assets/Scripts/GameJam/Portals.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portals : MonoBehaviour {
+
+    public int portalId;
+
+	void Start () {
+
+        PortalManager.AddPortals(this.gameObject);
+
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col) {
+
+        Debug.Log("toque");
+        PortalManager.ActivatePortal(this.gameObject, col.gameObject);
+
+    }
+}
