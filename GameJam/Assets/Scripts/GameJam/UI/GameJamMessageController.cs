427cb15bded3605a14b5e52a45845d41300605bf
Edgar JRG <erodriguez@teravisiongames.com>
2015-11-13 23:21:36 -0430

    id;

--- GameJam/Assets/Scripts/GameJam/UI/GameJamMessageController.cs
+++ GameJam/Assets/Scripts/GameJam/UI/GameJamMessageController.cs
@@ -3,108 +3,114 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
-public class GameJamMessageController : MonoBehaviour {
-	
-	public float lerpingTime = 1;
-	public float showedTime = 3;
+public class GameJamMessageController : MonoBehaviour
+{
 
-	public Text messageHolder;
-	private Vector3 hidePosition;
-	private Vector3 showPosition;
+    [SerializeField]
+    int ID = 0;
 
-	private float _lerpingTime;
+    public float lerpingTime = 1;
+    public float showedTime = 3;
 
-	private GameJamMessageServer _gms;
+    public Text messageHolder;
+    private Vector3 hidePosition;
+    private Vector3 showPosition;
 
-	public void Start()
-	{
-		//Gets the show and hide position
-		showPosition = transform.localPosition + Vector3.up * 100;
-		hidePosition = transform.localPosition;
-		transform.localPosition = hidePosition;
-		_lerpingTime = 1f / lerpingTime;
-	}
+    private float _lerpingTime;
 
-	public void Setup(GameJamMessageServer gms) {
-		_gms = gms;
-	}
+    private GameJamMessageServer _gms;
 
-	void Update() {
-		if (_gms == null)
-			_gms = FindObjectOfType<GameJamMessageServer>();
-		if (_gms.updateRed > 0)
-			checkForSpecificMessage(ref _gms.updateRed);
-		if (_gms.updateBlue > 0)
-			checkForSpecificMessage(ref _gms.updateBlue);
-		if (_gms.updateGreen > 0)
-			checkForSpecificMessage(ref _gms.updateGreen);
-		if (_gms.updateYellow > 0)
-			checkForSpecificMessage(ref _gms.updateYellow);
-	}
+    public void Start()
+    {
+        //Gets the show and hide position
+        showPosition = transform.localPosition + Vector3.up * 100;
+        hidePosition = transform.localPosition;
+        transform.localPosition = hidePosition;
+        _lerpingTime = 1f / lerpingTime;
+    }
 
-	void checkForSpecificMessage(ref int messageValue)
-	{
-		switch (messageValue)
-		{
-			case 1:
-				RpcReceiveMessage("Follow Me!");
-				break;
-			case 2:
-				RpcReceiveMessage("Whats this?");
-				break;
-			case 3:
-				RpcReceiveMessage("Action!");
-				break;
-			case 4:
-				RpcReceiveMessage("My Bad XD");
-				break;
-			case 5:
-				RpcReceiveMessage("NO!");
-				break;
-			case 6:
-				RpcReceiveMessage("Pushing Box");
-				break;
-			case 7:
-				RpcReceiveMessage("Broken Wall");
-				break;
-			case 8:
-				RpcReceiveMessage("Moving Platform");
-				break;
-			case 9:
-				RpcReceiveMessage("Portal");
-				break;
-			default:
-				break;
-		}
-		messageValue = 0;
-	}
+    public void Setup(GameJamMessageServer gms)
+    {
+        _gms = gms;
+    }
 
-	//[ClientRpc]
-	public void RpcReceiveMessage(string message)
-	{
-		messageHolder.text = message;
-		StopAllCoroutines();
-		transform.localPosition = hidePosition;
-		StartCoroutine(showMessage());
-	}
+    void Update()
+    {
+        if (_gms == null)
+            _gms = FindObjectOfType<GameJamMessageServer>();
+        if (this.ID == 1 && _gms.updateRed > 0)
+            checkForSpecificMessage(ref _gms.updateRed);
+        if (this.ID == 2 && _gms.updateBlue > 0)
+            checkForSpecificMessage(ref _gms.updateBlue);
+        if (this.ID == 3 && _gms.updateGreen > 0)
+            checkForSpecificMessage(ref _gms.updateGreen);
+        if (this.ID == 4 && _gms.updateYellow > 0)
+            checkForSpecificMessage(ref _gms.updateYellow);
+    }
 
-	public IEnumerator showMessage()
-	{
-		float time = 0;
-		while (time < lerpingTime)
-		{
-			time += Time.deltaTime;
-			transform.localPosition = Vector3.Lerp(hidePosition, showPosition, time * _lerpingTime);
-			yield return 0;
-		}
-		transform.localPosition = showPosition;
-		yield return new WaitForSeconds(showedTime);
-		while (time > 0)
-		{
-			time -= Time.deltaTime;
-			transform.localPosition = Vector3.Lerp(hidePosition, showPosition, time * _lerpingTime);
-			yield return 0;
-		}
-		transform.localPosition = hidePosition;
-	}
+    void checkForSpecificMessage(ref int messageValue)
+    {
+        switch (messageValue)
+        {
+            case 1:
+                RpcReceiveMessage("Follow Me!");
+                break;
+            case 2:
+                RpcReceiveMessage("Whats this?");
+                break;
+            case 3:
+                RpcReceiveMessage("Action!");
+                break;
+            case 4:
+                RpcReceiveMessage("My Bad XD");
+                break;
+            case 5:
+                RpcReceiveMessage("NO!");
+                break;
+            case 6:
+                RpcReceiveMessage("Pushing Box");
+                break;
+            case 7:
+                RpcReceiveMessage("Broken Wall");
+                break;
+            case 8:
+                RpcReceiveMessage("Moving Platform");
+                break;
+            case 9:
+                RpcReceiveMessage("Portal");
+                break;
+            default:
+                break;
+        }
+        messageValue = 0;
+    }
+
+    //[ClientRpc]
+    public void RpcReceiveMessage(string message)
+    {
+        messageHolder.text = message;
+        StopAllCoroutines();
+        transform.localPosition = hidePosition;
+        StartCoroutine(showMessage());
+    }
+
+    public IEnumerator showMessage()
+    {
+        float time = 0;
+        while (time < lerpingTime)
+        {
+            time += Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(hidePosition, showPosition, time * _lerpingTime);
+            yield return 0;
+        }
+        transform.localPosition = showPosition;
+        yield return new WaitForSeconds(showedTime);
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(hidePosition, showPosition, time * _lerpingTime);
+            yield return 0;
+        }
+        transform.localPosition = hidePosition;
+    }
 }
