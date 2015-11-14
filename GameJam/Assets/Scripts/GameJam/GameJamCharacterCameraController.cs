6fa67a21cd69ef83b3888cdb56626aac90865344
Edgar JRG <erodriguez@teravisiongames.com>
2015-11-13 20:09:13 -0430

    camera controller see only my color try3;

--- GameJam/Assets/Scripts/GameJam/GameJamCharacterCameraController.cs
+++ GameJam/Assets/Scripts/GameJam/GameJamCharacterCameraController.cs
@@ -7,37 +7,42 @@ using UnityEngine;
 public class GameJamCharacterCameraController : MonoBehaviour
 {
     #region Variables
+    GameJamCharacterSetUp setUp = null;
     Camera thisCamera = null;
     GameJamColors color = GameJamColors.White;
     #endregion
-    
-    #region Methods
 
+    #region Methods
+    
+    void Start()
+    {
+        Init();
+    }
 
     public void Init(bool isLocalPlayer, GameJamColors color)
     {
         thisCamera = this.GetComponentInChildren<Camera>();
         thisCamera.enabled = isLocalPlayer;
+
         if (isLocalPlayer)
         {
-            setLayerInCamera(color);
         }
         else
         {
-            
+
         }
+    }
+
+    public void Init()
+    {
+        setUp = this.GetComponent<GameJamCharacterSetUp>();
+        color = setUp.gjColor;
+        setLayerInCamera(color);
     }
 
     void setLayerInCamera(GameJamColors color)
     {
-        //int layer;
-
-        //layer = 1 << LayerMask.NameToLayer(color.EnumToLayerName());
-
-        //layer = ~layer;
-
         thisCamera.cullingMask = color.AllButThisCullingMask();
-
     }
 
     #endregion
