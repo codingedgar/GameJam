51193f714afeffb7165156d067807246b5ef1305
Edgar JRG <erodriguez@teravisiongames.com>
2015-11-13 19:41:43 -0430

    Camera sees only corresponding collor;

--- GameJam/Assets/Scripts/GameJam/GameJamCharacterCameraController.cs
+++ GameJam/Assets/Scripts/GameJam/GameJamCharacterCameraController.cs
@@ -8,6 +8,7 @@ public class GameJamCharacterCameraController : MonoBehaviour
 {
     #region Variables
     Camera thisCamera = null;
+    GameJamColors color = GameJamColors.White;
     #endregion
     
     #region Methods
@@ -19,14 +20,25 @@ public class GameJamCharacterCameraController : MonoBehaviour
         thisCamera.enabled = isLocalPlayer;
         if (isLocalPlayer)
         {
-            //thisCamera.gameObject.layer = LayerMask.NameToLayer(color.EnumToLayerName());
+            setLayerInCamera(color);
         }
         else
         {
-            //thisCamera.enabled = false;
+            
         }
     }
 
+    void setLayerInCamera(GameJamColors color)
+    {
+        //int layer;
+
+        //layer = 1 << LayerMask.NameToLayer(color.EnumToLayerName());
+
+        //layer = ~layer;
+
+        thisCamera.cullingMask = color.AllButThisCullingMask();
+
+    }
 
     #endregion
 }
