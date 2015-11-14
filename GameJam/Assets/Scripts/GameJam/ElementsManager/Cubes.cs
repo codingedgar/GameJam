37a70dc1e1a6444ae484eec338d150b0e06c4706
Brian Izaya <bizaya@teravisiongames.com>
2015-11-13 19:31:51 -0430

    Break wall

---
+++ GameJam/Assets/Scripts/GameJam/ElementsManager/Cubes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+
+	void Start () {
+
+        CubeManager.AddCube(this.gameObject);
+	
+	}
+	
+
+	void Update () {
+	
+	}
+}
---
+++ GameJam/Assets/Scripts/GameJam/Skills/DestroyWall.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyWall : MonoBehaviour {
+
+    private bool nearWall;
+    GameObject wall;
+
+	void Start () {
+
+
+	}
+	
+
+	void Update () {
+
+        ActivateBreak();
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        wall = BrokenWallManager.FindBrokenWall(other.gameObject);
+
+        if (other.gameObject == wall)
+        {
+            nearWall = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject == BrokenWallManager.FindBrokenWall(other.gameObject))
+        {
+            nearWall = false;
+        }
+    }
+
+    void ActivateBreak()
+    {
+
+        if (Input.GetKeyDown(KeyCode.P) && nearWall)
+        {
+            BrokenWallManager.BreakWall(wall);
+        }
+    }
+}
